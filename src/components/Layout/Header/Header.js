a89cd8b5f341c4797bbc90756f6f11ef0bee778f
Neeraj <admin@Admins-MacBook-Pro.local>
2022-06-13 21:08:40 +0100

    first commit

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,18 @@
+import classes from './Header.module.css'
+import mealsImg from '../../../assets/meals.jpeg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = (props) => {
+    return(
+    <>
+        <header className={classes.header}>
+            <h1>Food Order App</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImg} alt='table full of delicious food'/>
+        </div>
+    </>
+    )
+}
+export default Header;
